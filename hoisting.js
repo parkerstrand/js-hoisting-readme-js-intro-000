d1f3b82097f4373cfbd4374fcacaba8cb07fb451
Parker Strand <parker.strand@verizon.net>
2020-12-29 03:35:08 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,42 +1,20 @@
 function callMe() {
-var callMe = function callMe() {
-
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  lyric;
-  var lyric = "maybe";
   return lyric;
-  //put var lyric above return lyric
+  var lyric = "maybe";
 }
-callMe();
 
-// function crazy() {
-//   // fix the code in here:
-//   thisIsCrazy();
-//
-//   var thisIsCrazy = function (){
-//     console.log("hey!!!")
-//   }
-// }
-
-}
 
 function crazy() {
-var crazy = function() {
   // fix the code in here:
   thisIsCrazy();
 
   var thisIsCrazy = function (){
-  var thisIsCrazy = function() {
     console.log("hey!!!")
   }
-  thisIsCrazy();
-}
-crazy()
-
-}
 }
 
 function sayMyName() {
@@ -44,30 +22,10 @@ function sayMyName() {
   var name = "Cricky";
 
   sayMy();
-// function sayMyName() {
-//   // fix the code in here:
-//   var name = "Cricky";
-//
-//   function sayMy() {
-//     name = "Kristin"; // took out var before name here
-//     console.log(name);
-//   }
-//   sayMy();
-// }
-// sayMyName()()
-
-var sayMyName = function() {
-  // fix the code in here:
-  var name;
-  name = "Cricky";
 
   function sayMy() {
-    name = "Kristin"; // took out var before name here
+    name = "Kristin"; //
     console.log(name);
     var name = "Kristin";
   }
-  sayMy();
-}
-sayMyName()()
-
 }
